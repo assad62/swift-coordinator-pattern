da3fa9c7339604886086adbdd72dfec8cd67a2eb
Mohammad Assad <assad62@hotmail.com>
2024-10-05 21:21:33 -0400

    Initial Commit

---
+++ SwiftUICoordinators/SwiftUICoordinatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorsApp.swift
+//  SwiftUICoordinators
+//
+//  Created by Development on 5/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUICoordinatorsTests/SwiftUICoordinatorsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorsTests.swift
+//  SwiftUICoordinatorsTests
+//
+//  Created by Development on 5/10/2024.
+//
+
+import Testing
+@testable import SwiftUICoordinators
+
+struct SwiftUICoordinatorsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
